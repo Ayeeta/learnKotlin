7c9f7d1f4f3271c09e54bd7e3531437e611b559e
Elijah Ayeeta <ayeeta@awamo.com>
2020-03-12 16:36:19 +0300

    More on functions

--- Functions/defaultParameters.kt
+++ Functions/defaultParameters.kt
@@ -1,3 +1,11 @@
 fun printMessageWithDefaultParams(prefix: String="[", postfix: String="]"){
     println("$prefix $postfix")
 }
+
+fun noReturnValue(message: String): Unit{
+    println(message)
+}
+
+fun returnsInterger(x: Int, y: Int): Int{
+    return x + y
+}
